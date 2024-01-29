07600c1820cd25f558b2df0afc33c6745e533958
VIJI-SIST <157295000+VIJI-SIST@users.noreply.github.com>
2024-01-29 18:20:15 +0530

    Update ques1.py

--- 28-01-24/ques1.py
+++ 28-01-24/ques1.py
@@ -5,22 +5,23 @@ import re
 import sys
 
 #
-# Complete the 'miniMaxSum' function below.
+# Complete the 'maxMin' function below.
 #
-# The function accepts INTEGER_ARRAY arr as parameter.
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY arr
 #
 
 
-def miniMaxSum(arr):
+def maxMin(k, arr):
     # Write your code here
-    sum=0
-    for i in range(len(arr)):
-        sum=sum+arr[i]
-    print ( sum-max(arr), sum-min(arr))
-
-if __name__ == '__main__':
-
-    arr = list(map(int, input().rstrip().split()))
-
-    miniMaxSum(arr)
+    arr.sort()  
+  
+    min_unfair = float('infinity') 
+    
+    for i in range(len(arr) - k + 1):
+        unfairness = arr[i + k - 1] - arr[i]  
+        min_unfair = min(min_unfair, unfairness) 
+    return min_unfair
 
